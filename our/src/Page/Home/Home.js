1af412966a2c2edf847f8019e47f43cb943c83ee
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-09-19 15:18:42 +0500

    p

--- our/src/Page/Home/Home.js
+++ our/src/Page/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Master from "../../Component/Layouts/Master";
 import "../../Styles/Homestyle.css";
 import Sectionone from "./Sectionone";
+import { Sectiontwo } from "./Sectiontwo";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Master>
         {" "}
         <Sectionone></Sectionone>
+        <Sectiontwo></Sectiontwo>
       </Master>
     </>
   );
